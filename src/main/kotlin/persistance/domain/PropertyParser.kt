17affba07d4148de2fe1eb610b45ec20b5cc0af3
Konstantin Kolmogortsev <mr.kolmogortsev@gmail.com>
2024-11-28 20:59:30 +0300

    refactor: Divide files' content

---
+++ src/main/kotlin/persistance/domain/PropertyParser.kt
@@ -0,0 +1,50 @@
+package tira.persistance.domain
+
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.Reader
+import java.nio.charset.StandardCharsets
+import java.util.*
+
+class Parser() {
+    private val items = LinkedList<Token>()
+    fun parse(input: InputStream): List<Token> {
+        val br = BufferedReader(InputStreamReader(input, StandardCharsets.UTF_8))
+        var isBlockOpened = false
+
+        for (line in br.readLines().withIndex()) {
+            if (line.value.isEmpty() && line.value != "---") throw IllegalStateException("Must start with ---")
+            if (line.value == "---" && items.isEmpty()) {
+                isBlockOpened = true
+                continue
+            }
+            if (line.value == "---" && isBlockOpened) {
+                break
+            }
+
+            val tokens = line.value.split(":", ignoreCase = false, limit = 2).map { it.trim() }
+            if (tokens.size != 2) continue
+            items.add(ValueToken(tokens[0], tokens[1], line.index))
+
+        }
+        return items
+    }
+
+    private fun readBlock(r: Reader) {
+        val res = LinkedList<String>()
+    }
+}
+
+interface Token {
+    fun repr(): String
+    fun name(): String
+    fun linePos(): Int
+}
+
+class ValueToken(val name: String, val value: String, private val linePos: Int) : Token {
+    override fun repr(): String = "${name}: ${value}"
+    override fun name(): String = name
+
+    override fun linePos(): Int = linePos
+}
