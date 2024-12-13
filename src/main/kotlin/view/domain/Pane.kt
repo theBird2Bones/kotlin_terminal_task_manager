c0588ece4411028d61da65d8aeab10e09e84a24f
Konstantin Kolmogortsev <mr.kolmogortsev@gmail.com>
2024-12-13 15:42:43 +0300

    fix: Fix screen artefacts after switch to empty project

--- src/main/kotlin/view/domain/Pane.kt
+++ src/main/kotlin/view/domain/Pane.kt
@@ -75,6 +75,9 @@ abstract class AbstractListNavigationPane<A>(
     open fun draw() {
         if (items.isEmpty()) {
             printText("There is empty", cursor)
+            for (rowIdx in (cursor.row + 1 until size.height())) {
+                printText("", cursor.withRow(rowIdx))
+            }
             return
         }
 
